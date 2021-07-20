bf3fd343ec1871d199a917c837cc0058ad97dd0e
Dmitry <dzmitry.liashchynski@afrans.by>
2021-07-20 13:03:56 +0300

    Added console input/output

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -22,7 +22,7 @@ namespace HelloApp
              Console.WriteLine(a.GetType().ToString());
              Console.WriteLine(f.GetType().ToString());*/
 
-            string name = "Tom";
+           /* string name = "Tom";
             int age = 33;
             bool isEpmloyed = true;
             double weight = 78.65;
@@ -30,6 +30,29 @@ namespace HelloApp
             Console.WriteLine($"Age: {age}");
             Console.WriteLine($"Works: {isEpmloyed}");
             Console.WriteLine($"Weigh: {weight}");
+
+            Console.WriteLine($"Name: {name} Age: {age} Works: {isEpmloyed} Weight: {weight}");*/
+
+            Console.Write("Enter a name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your salary: ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+            
+            Console.WriteLine($"Name: {name}  Age: {age}  Height: {height}  Salary: {salary}$");
+            
+
+
+
+
+            Console.ReadKey();
 
         }
     }
