27fba17d782ce1f57235a04fb38b4bf15660fe52
Dmitry <dzmitry.liashchynski@afrans.by>
2021-07-20 11:51:03 +0300

    Learn basics of C#

---
+++ HelloApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");*/
+
+            /*string Name = "Tom";
+            Console.WriteLine(Name);*/
+
+            /* var hello = "Hello world";
+             var c = 20;
+             int a = 10;
+             ulong f = 20UL;
+             Console.WriteLine(c.GetType().ToString());
+             Console.WriteLine(hello.GetType().ToString());
+             Console.WriteLine(a.GetType().ToString());
+             Console.WriteLine(f.GetType().ToString());*/
+
+            string name = "Tom";
+            int age = 33;
+            bool isEpmloyed = true;
+            double weight = 78.65;
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Works: {isEpmloyed}");
+            Console.WriteLine($"Weight: {weight}");
+
+        }
+    }
+}
